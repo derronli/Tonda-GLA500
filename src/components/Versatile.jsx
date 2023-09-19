192e1392fd94b3c74be7757e6638798d5f23fa8b
Derron Li <104483680+derronli@users.noreply.github.com>
2023-09-18 23:47:39 -0400

    Modify Versatile.jsx component to be fully responsive

--- src/components/Versatile.jsx
+++ src/components/Versatile.jsx
@@ -7,14 +7,14 @@ const Versatile = () => {
     <div className="section-container pt-20 pb-28">
       <div
         style={{ backgroundImage: `url(${tes_versatile})` }}
-        className="w-full h-[650px] bg-no-repeat bg-cover bg-center"
+        className="w-full md:h-[650px] h-[450px] bg-no-repeat bg-cover bg-center"
       ></div>
-      <div className="text-wrapper flex flex-row">
-        <div className="w-1/6">
+      <div className="xl:mx-60 md:mx-36 xs:mx-20 mx-10 xl:pt-12 pt-8 flex sm:flex-row flex-col">
+        <div className="xl:w-1/6 w-3/12">
           <h1 className={styles.sideHeading}>VERSATILITY</h1>
         </div>
-        <div className="w-7/12">
-          <p className={`${styles.paragraph} text-[20px]`}>
+        <div className="md:w-7/12 xs:8/12 w-full md:pl-0 sm:pl-8 sm:pt-0 pt-4">
+          <p className={`${styles.paragraph} md:text-[20px] text-[16px]`}>
             It can take you new places, or make old favorite roads feel fresh.
             It can take up to six of your friends, or make easy work of the
             weekend chores. And wherever you take it, it'll outperform to make
@@ -22,10 +22,10 @@ const Versatile = () => {
           </p>
         </div>
       </div>
-      <div className="mx-60 pt-12">
+      <div className="xl:mx-60 md:mx-36 xs:mx-20 mx-10 pt-12">
         {" "}
         <div className="w-1/6">
-          <hr className="w-1/5 bg-white" />
+          <hr className="md:w-1/5 w-2/5 bg-white" />
         </div>
       </div>
     </div>
